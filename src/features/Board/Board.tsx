3bc9b67a074ea094fa758a2b3cf516d1799786eb
Bogdan Poshtarenko <62388820+mira1zu@users.noreply.github.com>
2022-09-07 17:39:45 +0200

    Phone experience (#7)
    * swipe implement
    
    * adaptiveness add

--- src/features/Board/Board.tsx
+++ src/features/Board/Board.tsx
@@ -13,6 +13,7 @@ import { move } from './state/thunks';
 import Direction from '../../ts/enums/Direction';
 
 import * as constants from '../../utils/constants';
+import Coords from '../../ts/types/Coords';
 
 const Board: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -20,23 +21,82 @@ const Board: React.FC = () => {
   const cells = useAppSelector(selectCells);
 
   const throttledMove = useCallback(_.throttle(
-    (event: KeyboardEvent) => {
-      dispatch(move(event.code as Direction));
+    (direction: Direction) => {
+      dispatch(move(direction));
     },
     constants.shiftAnimationLength,
   ), []);
 
   const keyboardListener = (event: KeyboardEvent) => {
     if (event.code.startsWith('Arrow')) {
-      throttledMove(event);
+      throttledMove(event.code as Direction);
+    }
+  };
+
+  const touchStartCoords: Coords = {
+    x: 0,
+    y: 0,
+  };
+  const touchEndCoords: Coords = {
+    x: 0,
+    y: 0,
+  };
+
+  const swipeStartListener = (event: TouchEvent) => {
+    const swipe = event.changedTouches.item(0);
+
+    if (!swipe) {
+      return;
+    }
+
+    touchStartCoords.x = swipe.clientX;
+    touchStartCoords.y = swipe.clientY;
+  };
+
+  const swipeEndListener = (event: TouchEvent) => {
+    const swipe = event.changedTouches.item(0);
+
+    if (!swipe) {
+      return;
+    }
+
+    touchEndCoords.x = swipe.clientX;
+    touchEndCoords.y = swipe.clientY;
+
+    const diffX = touchEndCoords.x - touchStartCoords.x;
+    const diffY = touchEndCoords.y - touchStartCoords.y;
+
+    switch (true) {
+      case diffX < 0 && Math.abs(diffX) > Math.abs(diffY):
+        throttledMove(Direction.Left);
+        break;
+
+      case diffX > 0 && Math.abs(diffX) > Math.abs(diffY):
+        throttledMove(Direction.Right);
+        break;
+
+      case diffY > 0 && Math.abs(diffX) < Math.abs(diffY):
+        throttledMove(Direction.Down);
+        break;
+
+      case diffY < 0 && Math.abs(diffX) < Math.abs(diffY):
+        throttledMove(Direction.Up);
+        break;
+
+      default:
+        break;
     }
   };
 
   useEffect(() => {
     document.addEventListener('keydown', keyboardListener);
+    document.addEventListener('touchstart', swipeStartListener);
+    document.addEventListener('touchend', swipeEndListener);
 
     return () => {
       document.removeEventListener('keydown', keyboardListener);
+      document.removeEventListener('touchstart', swipeStartListener);
+      document.removeEventListener('touchend', swipeEndListener);
     };
   }, []);
 
